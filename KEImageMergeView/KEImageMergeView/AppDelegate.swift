0576135685dba05d5d5a5efce69f5601c5e21c1e
Kaan Esin <kaanesin@teknasyon.com>
2021-01-08 23:18:11 +0300

    [init] pod and app initializations

---
+++ KEImageMergeView/KEImageMergeView/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  KEImageMergeView
+//
+//  Created by Kaan Esin on 8.01.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController else { return true }
+        self.window?.rootViewController = vc
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ KEImageMergeView/Pods/Target Support Files/Pods-KEImageMergeView/Pods-KEImageMergeView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KEImageMergeView : NSObject
+@end
+@implementation PodsDummy_Pods_KEImageMergeView
+@end
