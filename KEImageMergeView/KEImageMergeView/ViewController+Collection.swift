6a2a4846438d90f1bef43556ed2d511631fe8c66
Kaan Esin <kaanesin@teknasyon.com>
2021-01-09 19:12:12 +0300

    [refactor] extension for collection controller

---
+++ KEImageMergeView/KEImageMergeView/ViewController+Collection.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController+Collection.swift
+//  KEImageMergeView
+//
+//  Created by Kaan Esin on 9.01.2021.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.overlays?.count ?? 0) + 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OverlayCell", for: indexPath) as! OverlayCell
+        cell.tag = indexPath.row
+        
+        cell.imgCell.layer.masksToBounds = true
+        cell.imgCell.layer.cornerRadius = 10
+        cell.imgCell.layer.borderWidth = 3
+        
+        if indexPath.row == 0 {
+            cell.imgCell.image = #imageLiteral(resourceName: "noImage")
+            cell.lblCell.text = StaticKeys.Texts.None
+        }
+        else {
+            let item = self.overlays?[indexPath.row-1]
+            cell.item = item
+            if let overlayPreviewIconURL = item?.overlayPreviewIconURL {
+                cell.lblCell.text = item?.overlayName
+                cell.imgCell.kf.setImage(with: URL(string: overlayPreviewIconURL))
+            }
+        }
+        
+        if indexPath.row == self.selectedCellIndex {
+            cell.imgCell.layer.borderColor = UIColor(named: "RGB_87_163_215")?.cgColor
+            cell.lblCell.textColor = UIColor(named: "RGB_87_163_215")
+        }
+        else {
+            cell.imgCell.layer.borderColor = UIColor.clear.cgColor
+            cell.lblCell.textColor = UIColor(named: "RGB_81_80_80")
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.selectedCellIndex = indexPath.row
+        let offset = collectionView.contentOffset
+        self.collectionOverlay.reloadData()
+        self.collectionOverlay.layoutIfNeeded()
+        self.collectionOverlay.contentOffset = offset
+    }
+}
